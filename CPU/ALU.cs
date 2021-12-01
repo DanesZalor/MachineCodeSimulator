39640b1b2eb4281f141f892b2c88971fdbb41682
Daniel James Dolor <estheryahe@gmail.com>
2021-12-01 11:08:40 +0800

    added ALU_SHL testing and modified ALU.SHL() to modify Z flag

--- CPU/ALU.cs
+++ CPU/ALU.cs
@@ -44,7 +44,7 @@ namespace CPU{
         }
 
         /// <summary same as CMP(A,B) but also does A xor B and put the result in A </summary>
-        public void XOR(ref ushort A, ref ushort B){
+        public void XOR(ref ushort A, ushort B){
             CMP(A,B);
             A = (ushort)(A ^ B);
         }
@@ -56,27 +56,28 @@ namespace CPU{
         }
 
         /// <summary> changes A into conjunction of A and B. Affected flags [Z] </summary>
-        public void AND(ref ushort A, ref ushort B){
+        public void AND(ref ushort A, ushort B){
             A = (ushort)(A & B);
             setFlagsIf(A==0, FLAG.Z);
         }
 
         /// <summary> changes A into disjunction of A and B. Affected flags [Z] </summary>
-        public void OR(ref ushort A, ref ushort B){
+        public void OR(ref ushort A, ushort B){
             A = (ushort)(A | B);
             setFlagsIf(A==0, FLAG.Z);
         }
 
         /// <summary> changes A into Left shift A by B times. Affected flags [C] </summary>
-        public void SHL(ref ushort A, ref ushort B){
+        public void SHL(ref ushort A, ushort B){
             // CARRY ON if A<<B which is an INT32 is greater than the ushort.MaxValue
             int res = A<<B;
             setFlagsIf((res) > ushort.MaxValue, FLAG.C);
             A = (ushort)res;
+            setFlagsIf(A==0, FLAG.Z);
         }
 
         /// <summary> changes A into Right shift A by B times. Affected flags [C] </summary>
-        public void SHR(ref ushort A, ref ushort B){
+        public void SHR(ref ushort A, ushort B){
             // CARRY ON if A>>B is not equal to A/2^B
             int res = A>>B;
             setFlagsIf(res != A/(Math.Pow(2,B)), FLAG.C);
@@ -84,28 +85,28 @@ namespace CPU{
         }
 
         /// <summary> changes A into A / B. Affected flags [Z] </summary>
-        public void DIV(ref ushort A, ref ushort B){
+        public void DIV(ref ushort A, ushort B){
             if(B==0) return; // division by zero error
             
             A = (ushort)(A/B);
             setFlagsIf(A==0, FLAG.Z);
         }
         /// <summary> changes A into A * B. Affected flags [C,Z] </summary>
-        public void MUL(ref ushort A, ref ushort B){
+        public void MUL(ref ushort A, ushort B){
             int res = A*B;
             setFlagsIf(res==0, FLAG.Z);
             setFlagsIf(res>ushort.MaxValue, FLAG.C);
             A = (ushort)res;
         }
         /// <summary> changes A into A - B. Affected flags [C,Z] </summary>
-        public void SUB(ref ushort A, ref ushort B){
+        public void SUB(ref ushort A, ushort B){
             int res = A-B;
             setFlagsIf(res==0, FLAG.Z);
             setFlagsIf(res<0, FLAG.C);
             A = (ushort)res;
         }
         /// <summary> changes A into A + B. Affected flags [C,Z] </summary>
-        public void ADD(ref ushort A, ref ushort B){
+        public void ADD(ref ushort A, ushort B){
             int res = A+B;
             setFlagsIf(res==0, FLAG.Z);
             setFlagsIf(res>ushort.MaxValue, FLAG.C);
--- CPUTests/ALUTests.cs
+++ CPUTests/ALUTests.cs
@@ -45,7 +45,7 @@ namespace CPUTests{
         [InlineData(0b0011, 0b0011, 0b0000,ALU.FLAG.E | ALU.FLAG.Z)]
         [InlineData(0b0011, 0b1100, 0b1111)]
         public void ALU_XOR_modifiesFlagsCorrectlyAndChangesA(ushort A, ushort B, ushort expected_A, ALU.FLAG expected_flags = ALU.FLAG.OFF){
-            alu.XOR(ref A, ref B);
+            alu.XOR(ref A, B);
             Assert.Equal(A, expected_A);
             AssertFlags(alu,expected_flags);
         }
@@ -65,7 +65,7 @@ namespace CPUTests{
         [InlineData(0b1100, 0b0011, 0b0, ALU.FLAG.Z)]
         [InlineData(0b1011, 0b1010, 0b1010)]
         public void ALU_AND_changesACorrectly_andOnlyAffectzZFlag(ushort A, ushort B, ushort expected_A, ALU.FLAG expected_flags=ALU.FLAG.OFF){
-            alu.AND(ref A, ref B);
+            alu.AND(ref A, B);
             Assert.Equal(A,expected_A);
             AssertFlags(alu, expected_flags);
         }
@@ -74,9 +74,19 @@ namespace CPUTests{
         [InlineData(0b0, 0b0111, 0b0111)]
         [InlineData(0b0, 0b00, 0b0, ALU.FLAG.Z)]
         public void ALU_OR_changesACorrectly_andOnlyAffectzZFlag(ushort A, ushort B, ushort expected_A, ALU.FLAG expected_flags=ALU.FLAG.OFF){
-            alu.OR(ref A, ref B);
+            alu.OR(ref A, B);
             Assert.Equal(A,expected_A);
             AssertFlags(alu, expected_flags);
+        }
+
+        [Theory]
+        [InlineData(0b0011, 2, 0b1100)]
+        [InlineData(0b0011_0000_0000_0000, 3, 0b1000_0000_0000_0000, ALU.FLAG.C)]
+        [InlineData(0b0011_0000_0000_0000, 4, 0b0000_0000_0000_0000, ALU.FLAG.C | ALU.FLAG.Z)]
+        public void ALU_SHL_shiftsCorrectly_andOnlyAffectsCFlag(ushort A, ushort B, ushort expected_A, ALU.FLAG expected_flags=ALU.FLAG.OFF){
+            alu.SHL(ref A,B);
+            Assert.Equal(A,expected_A);
+            AssertFlags(alu,expected_flags);
         }
     }
 }
