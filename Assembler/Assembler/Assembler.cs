1c02614d6011e87bb2cd27350038e1e351b3ef0f
Daniel James Dolor <estheryahe@gmail.com>
2021-12-13 20:40:30 +0800

    added Instruction regex constants

--- Assembler/Assembler/Assembler.cs
+++ Assembler/Assembler/Assembler.cs
@@ -6,28 +6,35 @@ public static class Assembler
 {
 
     /// <summary> Regex constants for terms Register, Decimal, Offset, Address. Can be used for Regex.Match() </summary>
-    public static class Terms
+    public static class TERMS
     {
-        public const string Register = "([a-g]{1,2}|sp)";
-        public const string Decimal = "(" +
-                                        "(1[0-9]{0,2})|" +  // 1, 10-19, 100-199
-                                        "([1-9][0-9]?)|" +  // 1-99
-                                        "(2[0-4][0-9])|" +  // 200 - 249
-                                        "(25[0-5])" +       // 250-255
-                                    ")";
-        public const string Offset = "(" +
-                                        "(\\+([1-9]|(1[1-5])))|" +  // +1 to +15
-                                        "(-([1-9]|(1[1-6])))" +     // -1 to -16
-                                    ")";
-        public const string Address = "\\[(" +
-                                        Register + "|" +    // [Register]
-                                        Decimal + "|" +     // [Decimal]
-                                        "(" +               // [Register+Offset] or [Register-Offset]
-                                            Register + Offset +
-                                        ")" +
-                                    ")\\]";
+        public const string REGISTER = "([a-g]{1,2}|sp)";
+        public const string DECIMAL = "(" +
+            "(1[0-9]{0,2})|" +  // 1, 10-19, 100-199
+            "([1-9][0-9]?)|" +  // 1-99
+            "(2[0-4][0-9])|" +  // 200 - 249
+            "(25[0-5])" +       // 250-255
+        ")";
+        public const string OFFSET = "(" +
+            "(\\+([1-9]|(1[1-5])))|" +  // +1 to +15
+            "(-([1-9]|(1[1-6])))" +     // -1 to -16
+        ")";
+        public const string ADDRESS = "\\[(" +
+            REGISTER + "|" +    // [Register]
+            DECIMAL + "|" +     // [Decimal]
+            "(" +               // [Register+Offset] or [Register-Offset]
+                REGISTER + OFFSET +
+            ")" +
+        ")\\]";
+        public const string CONST = DECIMAL;
     }
 
+    /// <summary> Regex constants for Instruction sentences. Can be used for Regex.Match() </summary>
+    public static class INSTRUCTIONS
+    {
+        public const string MOV = "mov " + TERMS.REGISTER + "," + TERMS.REGISTER;
+        public const string DATA = "mov " + TERMS.REGISTER + "," + TERMS.CONST;
+    }
 
     public static bool match(string line, string pattern, bool exact = false)
     {
