ac29f6570004992e479167dfce531fe15f8cd6ee
Daniel James Dolor <estheryahe@gmail.com>
2021-12-14 09:27:30 +0800

    fixed register grammar

--- Assembler/Assembler/Assembler.cs
+++ Assembler/Assembler/Assembler.cs
@@ -5,36 +5,45 @@ namespace Assembler;
 public static class Assembler
 {
 
-    /// <summary> Regex constants for terms Register, Decimal, Offset, Address. Can be used for Regex.Match() </summary>
-    public static class TERMS
+    public static class LEXICON
     {
-        public const string REGISTER = "([a-g]{1,2}|sp)";
-        public const string DECIMAL = "(" +
-            "(1[0-9]{0,2})|" +  // 1, 10-19, 100-199
-            "([1-9][0-9]?)|" +  // 1-99
-            "(2[0-4][0-9])|" +  // 200 - 249
-            "(25[0-5])" +       // 250-255
-        ")";
-        public const string OFFSET = "(" +
-            "(\\+([1-9]|(1[1-5])))|" +  // +1 to +15
-            "(-([1-9]|(1[1-6])))" +     // -1 to -16
-        ")";
-        public const string ADDRESS = "\\[(" +
-            REGISTER + "|" +    // [Register]
-            DECIMAL + "|" +     // [Decimal]
-            "(" +               // [Register+Offset]
-                REGISTER + OFFSET +
-            ")" +
-        ")\\]";
-        public const string CONST = DECIMAL;
+        private const string SPACE = "(\\s)*";
+        private static class TOKENS
+        {
+            public const string REGISTER = "([a-g]|sp)";
+            public const string DECIMAL = "(" +
+                "(1[0-9]{0,2})|" +  // 1, 10-19, 100-199
+                "([1-9][0-9]?)|" +  // 1-99
+                "(2[0-4][0-9])|" +  // 200 - 249
+                "(25[0-5])" +       // 250-255
+            ")";
+            public const string OFFSET = "(" +
+                "(\\+([1-9]|(1[1-5])))|" +  // +1 to +15
+                "(-([1-9]|(1[1-6])))" +     // -1 to -16
+            ")";
+            public const string ADDRESS = "\\[(" +
+                REGISTER + "|" +    // [Register]
+                DECIMAL + "|" +     // [Decimal]
+                "(" +               // [Register+Offset]
+                    REGISTER + OFFSET +
+                ")" +
+            ")\\]";
+            public const string CONST = DECIMAL;
+        }
+
+        public static class SYNTAX
+        {
+            public const string MOV = "mov " + SPACE + TOKENS.REGISTER + SPACE + "," + SPACE + TOKENS.REGISTER + SPACE;
+            public const string DATA = "mov " + SPACE + TOKENS.REGISTER + SPACE + "," + SPACE + TOKENS.CONST + SPACE;
+        }
+
+        public static class VAGUE
+        {
+            private const string REGISTER = "(( [a-z]+)|(,[a-z])+)";
+            public const string MOV = "mov " + SPACE + REGISTER + SPACE + "," + SPACE + REGISTER + SPACE;
+        }
     }
 
-    /// <summary> Regex constants for Instruction sentences. Can be used for Regex.Match() </summary>
-    public static class INSTRUCTIONS
-    {
-        public const string MOV = "mov " + TERMS.REGISTER + "," + TERMS.REGISTER;
-        public const string DATA = "mov " + TERMS.REGISTER + "," + TERMS.CONST;
-    }
 
     public static bool match(string line, string pattern, bool exact = false)
     {
@@ -42,21 +51,27 @@ public static class Assembler
         return Regex.Match(line, pattern, RegexOptions.IgnoreCase).Success;
     }
 
-    public static string evaluateLine(string line)
+    public static string evaluate_MOV(string line)
     {
-        string error_msg = "";
+        string error_msg = line;
+
+        if (!match(line, LEXICON.SYNTAX.MOV))
+        {
+            //Match m = Regex.Match(line, LEXICON.VAGUE.MOV);
+            //for (int i = 0; i < m.Length; i++) error_msg += String.Format("\"{0}\" is not a valid Register", (i == 0 ? m.Value : m.NextMatch()));
+            error_msg = "wrong grammar";
+        }
         return error_msg;
     }
 
-    public static byte[] TranslateLine(string line)
+    public static string evaluateLine(string line)
     {
+        string res = "";
+        if (match(line, "^mov "))// Start with "mov "
+        {
 
-        return new byte[1];
-    }
-    public static byte[] Compile(string code)
-    {
-
-        return new byte[1];
+        }
+        return res;
     }
 
 
--- Assembler/AssemblerTest/AssemblerTest.cs
+++ Assembler/AssemblerTest/AssemblerTest.cs
@@ -9,60 +9,24 @@ namespace AssemblerTest;
 public class AssemblerTest
 {
     [Theory]
-    // REGISTER testing
-    [InlineData("a", Assembler.Assembler.TERMS.REGISTER)]
-    [InlineData("b", Assembler.Assembler.TERMS.REGISTER)]
-    [InlineData("c", Assembler.Assembler.TERMS.REGISTER)]
-    [InlineData("d", Assembler.Assembler.TERMS.REGISTER)]
-    [InlineData("e", Assembler.Assembler.TERMS.REGISTER)]
-    [InlineData("f", Assembler.Assembler.TERMS.REGISTER)]
-    [InlineData("g", Assembler.Assembler.TERMS.REGISTER)]
-    [InlineData("h", Assembler.Assembler.TERMS.REGISTER, false)]
-    [InlineData("sp", Assembler.Assembler.TERMS.REGISTER)]
-    [InlineData("G", Assembler.Assembler.TERMS.REGISTER)]
-    // DECIMALs
-    [InlineData("155", Assembler.Assembler.TERMS.DECIMAL)]
-    [InlineData("1", Assembler.Assembler.TERMS.DECIMAL)]
-    [InlineData("19", Assembler.Assembler.TERMS.DECIMAL)]
-    [InlineData("1672", Assembler.Assembler.TERMS.DECIMAL, false)]
-    [InlineData("29", Assembler.Assembler.TERMS.DECIMAL)]
-    [InlineData("2", Assembler.Assembler.TERMS.DECIMAL)]
-    [InlineData("22", Assembler.Assembler.TERMS.DECIMAL)]
-    [InlineData("219", Assembler.Assembler.TERMS.DECIMAL)]
-    [InlineData("256", Assembler.Assembler.TERMS.DECIMAL, false)]
-    [InlineData("279", Assembler.Assembler.TERMS.DECIMAL, false)]
-    [InlineData("255", Assembler.Assembler.TERMS.DECIMAL)]
-    [InlineData("1100", Assembler.Assembler.TERMS.DECIMAL, false)]
-    // OFFSET
-    [InlineData("+1", Assembler.Assembler.TERMS.OFFSET)]
-    [InlineData("+5", Assembler.Assembler.TERMS.OFFSET)]
-    [InlineData("+14", Assembler.Assembler.TERMS.OFFSET)]
-    [InlineData("+16", Assembler.Assembler.TERMS.OFFSET, false)]
-    [InlineData("+0", Assembler.Assembler.TERMS.OFFSET, false)]
-    [InlineData("-1", Assembler.Assembler.TERMS.OFFSET)]
-    [InlineData("-5", Assembler.Assembler.TERMS.OFFSET)]
-    [InlineData("-13", Assembler.Assembler.TERMS.OFFSET)]
-    [InlineData("-16", Assembler.Assembler.TERMS.OFFSET)]
-    [InlineData("-17", Assembler.Assembler.TERMS.OFFSET, false)]
-    // ADDRESS
-    [InlineData("[a]", Assembler.Assembler.TERMS.ADDRESS)]
-    [InlineData("[c]", Assembler.Assembler.TERMS.ADDRESS)]
-    [InlineData("[201]", Assembler.Assembler.TERMS.ADDRESS)]
-    [InlineData("[D+12]", Assembler.Assembler.TERMS.ADDRESS)]
-    [InlineData("[D+15]", Assembler.Assembler.TERMS.ADDRESS)]
-    [InlineData("[D+16]", Assembler.Assembler.TERMS.ADDRESS, false)]
-    [InlineData("[D-0]", Assembler.Assembler.TERMS.ADDRESS, false)]
-    [InlineData("[D+0]", Assembler.Assembler.TERMS.ADDRESS, false)]
-    [InlineData("[D-1]", Assembler.Assembler.TERMS.ADDRESS)]
-    [InlineData("[D-9]", Assembler.Assembler.TERMS.ADDRESS)]
-    [InlineData("[D-16]", Assembler.Assembler.TERMS.ADDRESS)]
-    [InlineData("[D-17]", Assembler.Assembler.TERMS.ADDRESS, false)]
-    // Instructions
-    [InlineData("mov a,a", Assembler.Assembler.INSTRUCTIONS.MOV)]
+    [InlineData("mov a, sp", Assembler.Assembler.LEXICON.SYNTAX.MOV)]
+    [InlineData("mov G,  B", Assembler.Assembler.LEXICON.SYNTAX.MOV)]
+    [InlineData("mov C,D", Assembler.Assembler.LEXICON.SYNTAX.MOV)]
+    [InlineData("mov     E     ,      F     ", Assembler.Assembler.LEXICON.SYNTAX.MOV)]
+    [InlineData("movE,F", Assembler.Assembler.LEXICON.SYNTAX.MOV, false)]
     public void testMatch(string line, string pattern, bool res = true)
     {
         bool actual_res = Assembler.Assembler.match(line, pattern, true);
         Assert.Equal(actual_res, res);
+    }
+
+    [Fact]
+    public void testMov()
+    {
+        string s = "";
+        s = Assembler.Assembler.evaluate_MOV("mov a, b"); Console.WriteLine(s);
+        s = Assembler.Assembler.evaluate_MOV("mov a, asasf"); Console.WriteLine(s);
+
     }
 
 }
