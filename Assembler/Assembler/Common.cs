0baba4f5864536b88d76c4150a1ec54aac72f4a9
Daniel James Dolor <estheryahe@gmail.com>
2022-01-28 15:36:31 +0800

    adding comments

--- Assembler/Assembler/Common.cs
+++ Assembler/Assembler/Common.cs
@@ -1,13 +1,17 @@
 namespace Assembler;
 using System.Text.RegularExpressions;
 
+/// <summary> Contains common stuff </summary>
 public static class Common
 {
+    /// <summary> get matches from line that satisfy the grammar pattern </summary>
     public static Match getMatch(string line, string pattern, bool exact = false)
     {
         if (exact) pattern = "^" + pattern + "$";
         return Regex.Match(line, pattern, RegexOptions.IgnoreCase);
     }
+
+    /// <summary> returns true if line satisfy the grammar pattern </summary>
     public static bool match(string line, string pattern, bool exact = false)
     {
         return getMatch(line, pattern, exact).Success;
