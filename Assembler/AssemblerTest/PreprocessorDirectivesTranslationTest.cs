595f5e3ab5e792fa052a30a4a14b4ea348be71c5
Daniel James Dolor <estheryahe@gmail.com>
2022-02-05 18:42:58 +0800

    added this test case

--- Assembler/AssemblerTest/PreprocessorDirectivesTranslationTest.cs
+++ Assembler/AssemblerTest/PreprocessorDirectivesTranslationTest.cs
@@ -6,6 +6,7 @@ public class PreprocessorDirectivesTest
     [InlineData("correct1")]
     [InlineData("correct2")]
     [InlineData("correct3")]
+    [InlineData("correct4")]
     [InlineData("correct7")]
     [InlineData("correct8")]
     [InlineData("correct10")]
