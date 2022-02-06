e9f92dee162a496850de68cdcc9b8c3d32c029ac
Daniel James Dolor <estheryahe@gmail.com>
2022-02-06 09:19:23 +0800

    removing these. we don't need it anymore

--- Assembler/Assembler/LEXICON.cs
+++ Assembler/Assembler/LEXICON.cs
@@ -63,33 +63,7 @@ public static class LEXICON
                 A_R + "|" +
             ")";
         }
-
-        public const string MOV = SPACE + "mov " + ARGUEMENTS.DYADIC;
-        ///<summary> mov reg, reg </summary> 
-        public const string MOV_R_R = SPACE + "mov " + ARGUEMENTS.R_R;
-
-        ///<summary> mov reg, const </summary> 
-        public const string MOV_R_C = SPACE + "mov " + ARGUEMENTS.R_C;
-
-
-        public const string MOV_R_A = SPACE + "mov " + ARGUEMENTS.R_A;
-
-        public const string MOV_A_R = SPACE + "mov " + ARGUEMENTS.A_R;
-
-        ///<summary> jmp reg </summary>
-        public const string JMP_R = SPACE + "jmp " + ARGUEMENTS.R;
-        ///<summary> jmp const </summary>
-        public const string JMP_C = SPACE + "jmp " + ARGUEMENTS.C;
-
-
         public const string JCAZ = "(j((ca?z?)|(c?az?)|(c?a?z)))";
         ///<summary> jcaz reg </summary>
-        public const string JCAZ_R= SPACE + JCAZ + " " + ARGUEMENTS.R;
-        ///<summary> jcaz const </summary>
-        public const string JCAZ_C = SPACE + JCAZ + " " + ARGUEMENTS.C;
-
-        public const string PUSH_R = SPACE + "push " + ARGUEMENTS.R;
-        public const string PUSH_A = SPACE + "push " + ARGUEMENTS.A;
-        public const string PUSH_C = SPACE + "push " + ARGUEMENTS.C;
     }
 }
