acf398815aba522e0248b90814e686bae4a7d24c
Daniel James Dolor <estheryahe@gmail.com>
2022-02-17 15:12:02 +0800

    ay puta naka upper case diay abi nako naay bug

--- Assembler/AssemblerTest/TranlatorTest.cs
+++ Assembler/AssemblerTest/TranlatorTest.cs
@@ -115,11 +115,24 @@ public class TranslationCheck
     [Theory]
     [InlineData("BinTest/test1", new byte[5]{0b1000, 0b1010, 0b1001, 0b10, 0b1101_0000})]
     [InlineData("BinTest/jo1", new byte[12]{0b1000, 1, 0b1001, 2, 0b1100_0000, 0b0000_0001, 0b0100_1101, 9, 0b1101_0010, 0b10001_000, 0b0011_1000, 4})]
-    [InlineData("BinTest/jo2", new byte[22]{0b1000, 1, 0b1001, 2, 0b0011_1000, 9, 0b10001_000, 0b0011_1000, 9, 0b1100_0000, 0b0000_0001, 0b0100_1101, 6, 0b1000, 253, 0b1001, 1, 0b1100_1000, 0b0000_0001, 0100_1011, 24, 1101_0010})]
+    [InlineData("BinTest/jo2", new byte[22]{0b1000, 1, 0b1001, 2, 0b0011_1000, 9, 0b10001_000, 0b0011_1000, 9, 0b1100_0000, 0b0000_0001, 0b0100_1101, 6, 0b1000, 253, 0b1001, 1, 0b1100_1000, 0b0000_0001, 0b0100_1011, 24, 0b1101_0010})]
     [InlineData("BinTest/jo3", new byte[27]{0b1000, 0, 0b1001, 0, 0b1100_0010, 0b0000_0001, 0b0100_1110, 14, 0b10001_000, 0b1100_1000, 0b0001_1010, 3, 0b0011_1000, 4, 0b1000, 1, 0b1001, 1, 0b1100_0001, 0b0000_0001, 0b1000, 14, 0b1001, 20, 0b1100_0011, 0b0000_0001, 0b1101_0010})]
     public void TestCompileFile(string filename, byte[] expected_res){
+        void assertEqual(byte[] a, byte[] b){
+            Assert.Equal(a.Length, b.Length);
+            //for(int i = 0; i<a.Length; i++) Assert.True(a[i]==b[i], Convert.ToString(i) + " index " +Convert.ToString(a[i]) + " and " + Convert.ToString(b[i]) );
+            bool arrayEqual = true;
+            for(int i = 0; i<a.Length; i++){
+                if(a[i]!=b[i]) arrayEqual = false;
+            }
+            if(!arrayEqual){
+                Console.Write("a = ["); for(int i = 0; i < a.Length; i++) Console.Write(a[i] + ","); Console.WriteLine(" size:" +Convert.ToString(a.Length));
+                Console.Write("b = ["); for(int i = 0; i < b.Length; i++) Console.Write(b[i] + ","); Console.WriteLine(" size:" +Convert.ToString(b.Length));
+            }
+        }
+
         byte[] actual_res = Assembler.Translator.translateProgram(Common.readFile(filename));
-        Assert.Equal(expected_res, actual_res);
+        assertEqual(expected_res, actual_res);
     }
     
 }
